c6b0a495007a2543ea87b22f8624e26fc5c160e2
sajar-mohammed <sajarmohammed71@gmail.com>
2026-02-18 16:26:05 +0530

    updated code

--- dist/common/decorators/skip-response.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const SKIP_RESPONSE_METADATA = "skipResponse";
-export declare const SkipResponse: () => import("@nestjs/common").CustomDecorator<string>;
--- dist/common/interceptors/response/response.interceptor.d.ts
+++
@@ -1,8 +0,0 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { Reflector } from '@nestjs/core';
-export declare class ResponseInterceptor implements NestInterceptor {
-    private readonly reflector;
-    constructor(reflector: Reflector);
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any>;
-}
